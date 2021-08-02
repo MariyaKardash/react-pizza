c1bb4c7da1d8b59050c65a8bdb1cd83ed5ada9fd
Mariya Kardash <mariya.kardash2@gmail.com>
2021-08-02 13:43:57 +0300

    create pizza block component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Route } from 'react-router-dom';
 
 import { Header } from './components';
 import { Home, Cart } from './pages';
 
 function App() {
+  const [pizzas, setPizzas] = useState([]);
+  useEffect(() => {
+    fetch('http://localhost:3000/db.json')
+      .then((res) => res.json())
+      .then((data) => setPizzas(data.pizzas));
+  }, []);
+
+  console.log(pizzas);
+
   return (
     <div className="wrapper">
       <Header />
