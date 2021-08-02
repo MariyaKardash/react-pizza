4a790ddd85e9abb0a69bc3f20b920cbf40a4a76a
Mariya Kardash <mariya.kardash2@gmail.com>
2021-08-02 14:36:41 +0300

    add axios request

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 import { Route } from 'react-router-dom';
 
@@ -7,9 +8,9 @@ import { Home, Cart } from './pages';
 function App() {
   const [pizzas, setPizzas] = useState([]);
   useEffect(() => {
-    fetch('http://localhost:3000/db.json')
-      .then((res) => res.json())
-      .then((data) => setPizzas(data.pizzas));
+    axios.get('http://localhost:3000/db.json').then(({ data }) => {
+      setPizzas(data.pizzas);
+    });
   }, []);
 
   return (
